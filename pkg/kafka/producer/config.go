3454ef547869126896a371c08cef517e0cc71d8d
Joni Collinge <jonathancollinge@live.com>
2018-09-04 10:15:52 +0100

    Added flags to support more sarama configuration options

--- pkg/kafka/producer/config.go
+++ pkg/kafka/producer/config.go
@@ -15,8 +15,29 @@
 package producer
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+
 	"github.com/Shopify/sarama"
 )
+
+// TLS Conifg
+type TLS struct {
+	Enabled                bool
+	CertPath               string
+	KeyPath                string
+	CaPath                 string
+	EnableHostVerification bool
+}
+
+// SASLAuthenticator holds the username and password used for SASL authentication
+type SASLAuthenticator struct {
+	Enabled  bool   `yaml:"enabled"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
 
 // Builder builds a new kafka producer
 type Builder interface {
@@ -25,12 +46,83 @@ type Builder interface {
 
 // Configuration describes the configuration properties needed to create a Kafka producer
 type Configuration struct {
-	Brokers []string
+	Brokers      []string
+	SASL         SASLAuthenticator
+	TLS          TLS
+	MetadataFull bool
+	Version      string
 }
 
 // NewProducer creates a new asynchronous kafka producer
 func (c *Configuration) NewProducer() (sarama.AsyncProducer, error) {
-	saramaConfig := sarama.NewConfig()
-	saramaConfig.Producer.Return.Successes = true
+	saramaConfig, err := NewConfig(c)
+	if err != nil {
+		return nil, err
+	}
 	return sarama.NewAsyncProducer(c.Brokers, saramaConfig)
+}
+
+// NewConfig creates a new sarama configuration
+func NewConfig(c *Configuration) (*sarama.Config, error) {
+	cfg := sarama.NewConfig()
+	cfg.Producer.Return.Successes = true
+	if c.SASL.Enabled {
+		cfg.Net.SASL.Enable = true
+		cfg.Net.SASL.User = c.SASL.Username
+		cfg.Net.SASL.Password = c.SASL.Password
+	}
+	if c.TLS.Enabled {
+		cfg.Net.TLS.Enable = true
+		tlsConfig, err := NewTLSConfig(c.TLS.CertPath, c.TLS.KeyPath, c.TLS.CaPath, !c.TLS.EnableHostVerification)
+		if err != nil {
+			return nil, fmt.Errorf("error creating TLS config: %+v", err)
+		}
+		cfg.Net.TLS.Config = tlsConfig
+	}
+	if c.Version == "" {
+		cfg.Version = sarama.MinVersion
+	} else {
+		v, err := sarama.ParseKafkaVersion(c.Version)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing kafka version: %+v", err)
+		}
+		cfg.Version = v
+	}
+	cfg.Metadata.Full = c.MetadataFull
+	return cfg, nil
+}
+
+// NewTLSConfig constructs a new TLS config
+func NewTLSConfig(clientCertFile, clientKeyFile, caCertFile string, insecureSkipVerify bool) (*tls.Config, error) {
+	var err error
+
+	tlsConfig := tls.Config{}
+	clientAuth := tls.NoClientCert
+
+	caCertPool := x509.NewCertPool()
+	if caCertFile != "" {
+		caCert, err := ioutil.ReadFile(caCertFile) // #nosec
+		if err != nil {
+			return &tlsConfig, err
+		}
+		caCertPool.AppendCertsFromPEM(caCert)
+		tlsConfig.RootCAs = caCertPool
+	}
+
+	cert := tls.Certificate{}
+	if clientCertFile != "" && clientKeyFile != "" {
+		cert, err = tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
+		if err != nil {
+			return &tlsConfig, err
+		}
+	}
+
+	/* #nosec */
+	tlsConfig = tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: insecureSkipVerify,
+		ClientAuth:         clientAuth,
+	}
+	return &tlsConfig, nil
 }
--- plugin/storage/kafka/options.go
+++ plugin/storage/kafka/options.go
@@ -25,17 +25,28 @@ import (
 )
 
 const (
-	configPrefix   = "kafka"
-	suffixBrokers  = ".brokers"
-	suffixTopic    = ".topic"
-	suffixEncoding = ".encoding"
+	configPrefix       = "kafka"
+	suffixBrokers      = ".brokers"
+	suffixTopic        = ".topic"
+	suffixEncoding     = ".encoding"
+	suffixSasl         = ".sasl"
+	suffixSaslUsername = ".sasl.username"
+	suffixSaslPassword = ".sasl.password"
+	suffixMetadataFull = ".metadata.full"
+	suffixTLS          = ".tls"
+	suffixCert         = ".tls.cert"
+	suffixKey          = ".tls.key"
+	suffixCA           = ".tls.ca"
+	suffixVerifyHost   = ".tls.verify-host"
+	suffixVersion      = ".version"
 
 	encodingJSON  = "json"
 	encodingProto = "protobuf"
 
-	defaultBroker   = "127.0.0.1:9092"
-	defaultTopic    = "jaeger-spans"
-	defaultEncoding = encodingProto
+	defaultBroker       = "127.0.0.1:9092"
+	defaultTopic        = "jaeger-spans"
+	defaultEncoding     = encodingProto
+	defaultMetadataFull = true
 )
 
 // Options stores the configuration options for Kafka
@@ -60,13 +71,75 @@ func (opt *Options) AddFlags(flagSet *flag.FlagSet) {
 		defaultEncoding,
 		fmt.Sprintf(`Encoding of spans ("%s" or "%s") sent to kafka.`, encodingProto, encodingJSON),
 	)
+	flagSet.Bool(
+		configPrefix+suffixSasl,
+		opt.config.SASL.Enabled,
+		"Enable SASL",
+	)
+	flagSet.String(
+		configPrefix+suffixSaslUsername,
+		opt.config.SASL.Username,
+		"SASL username",
+	)
+	flagSet.String(
+		configPrefix+suffixSaslPassword,
+		opt.config.SASL.Password,
+		"SASL password",
+	)
+	flagSet.Bool(
+		configPrefix+suffixMetadataFull,
+		defaultMetadataFull,
+		"Full metadata for all topics (default=true)",
+	)
+	flagSet.Bool(
+		configPrefix+suffixTLS,
+		opt.config.TLS.Enabled,
+		"Enable TLS",
+	)
+	flagSet.String(
+		configPrefix+suffixCert,
+		opt.config.TLS.CertPath,
+		"Path to TLS certificate file",
+	)
+	flagSet.String(
+		configPrefix+suffixKey,
+		opt.config.TLS.KeyPath,
+		"Path to TLS key file",
+	)
+	flagSet.String(
+		configPrefix+suffixCA,
+		opt.config.TLS.CaPath,
+		"Path to TLS CA file",
+	)
+	flagSet.Bool(
+		configPrefix+suffixVerifyHost,
+		opt.config.TLS.EnableHostVerification,
+		"Enable (or disable) host key verification",
+	)
+	flagSet.String(
+		configPrefix+suffixVersion,
+		opt.config.Version,
+		"Kafka API version",
+	)
 }
 
 // InitFromViper initializes Options with properties from viper
 func (opt *Options) InitFromViper(v *viper.Viper) {
-	opt.config = producer.Configuration{
-		Brokers: strings.Split(v.GetString(configPrefix+suffixBrokers), ","),
-	}
+	var cfg producer.Configuration
+
+	cfg.SASL.Enabled = v.GetBool(configPrefix + suffixSasl)
+	cfg.SASL.Username = v.GetString(configPrefix + suffixSaslUsername)
+	cfg.SASL.Password = v.GetString(configPrefix + suffixSaslPassword)
+	cfg.MetadataFull = v.GetBool(configPrefix + suffixMetadataFull)
+	cfg.TLS.Enabled = v.GetBool(configPrefix + suffixTLS)
+	cfg.TLS.CertPath = v.GetString(configPrefix + suffixCert)
+	cfg.TLS.KeyPath = v.GetString(configPrefix + suffixKey)
+	cfg.TLS.CaPath = v.GetString(configPrefix + suffixCA)
+	cfg.TLS.EnableHostVerification = v.GetBool(configPrefix + suffixVerifyHost)
+	cfg.Brokers = strings.Split(v.GetString(configPrefix+suffixBrokers), ",")
+	cfg.Version = v.GetString(configPrefix + suffixVersion)
+
+	opt.config = cfg
 	opt.topic = v.GetString(configPrefix + suffixTopic)
 	opt.encoding = v.GetString(configPrefix + suffixEncoding)
 }
