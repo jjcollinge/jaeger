50b5c114ab3c7f03ddb3e3e8a8bfc0d5553faf3d
Joni Collinge <jonathancollinge@live.com>
2018-09-04 16:32:33 +0100

    Added unit tests for additional sarama config

--- plugin/storage/kafka/options_test.go
+++ plugin/storage/kafka/options_test.go
@@ -28,12 +28,32 @@ func TestOptionsWithFlags(t *testing.T) {
 	command.ParseFlags([]string{
 		"--kafka.topic=topic1",
 		"--kafka.brokers=127.0.0.1:9092,0.0.0:1234",
-		"--kafka.encoding=protobuf"})
+		"--kafka.encoding=protobuf",
+		"--kafka.sasl=true",
+		"--kafka.sasl.username=username",
+		"--kafka.sasl.password=password",
+		"--kafka.metadata.full=false",
+		"--kafka.tls=true",
+		"--kafka.tls.cert=cert",
+		"--kafka.tls.key=key",
+		"--kafka.tls.ca=ca",
+		"--kafka.tls.verify-host=true",
+		"--kafka.version=1.0.2"})
 	opts.InitFromViper(v)
 
 	assert.Equal(t, "topic1", opts.topic)
 	assert.Equal(t, []string{"127.0.0.1:9092", "0.0.0:1234"}, opts.config.Brokers)
 	assert.Equal(t, "protobuf", opts.encoding)
+	assert.Equal(t, true, opts.config.SASL.Enabled)
+	assert.Equal(t, "username", opts.config.SASL.Username)
+	assert.Equal(t, "password", opts.config.SASL.Password)
+	assert.Equal(t, false, opts.config.MetadataFull)
+	assert.Equal(t, true, opts.config.TLS.Enabled)
+	assert.Equal(t, "cert", opts.config.TLS.CertPath)
+	assert.Equal(t, "key", opts.config.TLS.KeyPath)
+	assert.Equal(t, "ca", opts.config.TLS.CaPath)
+	assert.Equal(t, true, opts.config.TLS.EnableHostVerification)
+	assert.Equal(t, "1.0.2", opts.config.Version)
 }
 
 func TestFlagDefaults(t *testing.T) {
@@ -45,4 +65,14 @@ func TestFlagDefaults(t *testing.T) {
 	assert.Equal(t, defaultTopic, opts.topic)
 	assert.Equal(t, []string{defaultBroker}, opts.config.Brokers)
 	assert.Equal(t, defaultEncoding, opts.encoding)
+	assert.Equal(t, false, opts.config.SASL.Enabled)
+	assert.Equal(t, "", opts.config.SASL.Username)
+	assert.Equal(t, "", opts.config.SASL.Password)
+	assert.Equal(t, true, opts.config.MetadataFull)
+	assert.Equal(t, false, opts.config.TLS.Enabled)
+	assert.Equal(t, "", opts.config.TLS.CertPath)
+	assert.Equal(t, "", opts.config.TLS.KeyPath)
+	assert.Equal(t, "", opts.config.TLS.CaPath)
+	assert.Equal(t, false, opts.config.TLS.EnableHostVerification)
+	assert.Equal(t, "", opts.config.Version)
 }
